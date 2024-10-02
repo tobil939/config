f977c23cdf75a5f7022c1060a635064fb9bf4503
tobil939 <tobias.leitz@gmx.de>
2024-10-02 19:01:01 +0200

    korrigiert von chatgpt

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Installation von verschiedenen Paketen
-echo -e "\n \n \ \n neofetch"
+echo -e "\n \n \n neofetch"
 sudo pacman -S neofetch --noconfirm
 echo -e "\n \n \n kitty"
 sudo pacman -S kitty --noconfirm
@@ -11,7 +11,7 @@ echo -e "\n \n \n firefox"
 sudo pacman -S firefox --noconfirm
 echo -e "\n \n \n gedit"
 sudo pacman -S gedit --noconfirm
-echo -e "\n \n \n nttf-meslo-nerd"
+echo -e "\n \n \n ttf-meslo-nerd"
 sudo pacman -S ttf-meslo-nerd --noconfirm
 echo -e "\n \n \n picom"
 sudo pacman -S picom --noconfirm
@@ -47,118 +47,91 @@ echo -e "\n \n \n Kalender"
 sudo pacman -S gnome-calendar --noconfirm
 echo -e "\n \n \n Nvim"
 sudo pacman -S neovim --noconfirm
-echo -e "\n \n \n Email"
-#sudo pacman -S gnome-extra --noconfirm
-#echo -e "\n \n \n yay"
-#sudo pacman -S -needed base-devel git --noconfirm
-#git clone https://aur.archlinux.org/yay-bin.git
-#cd yay-bin
-#makepkg -si
-#yay -version
 
+# Update durchführen
 echo -e "\n \n \n Update"
-sudo pacman -Syu -noconfirm
+sudo pacman -Syu --noconfirm
 
+# Git-Konfiguration
 echo -e "\n \n \n git config"
 git config --global user.name "tobil939"
 git config --global user.email "tobias.leitz@gmx.de"
 git config --global core.editor nvim
 git config --list
 
+# Git-Ordner erstellen und klonen
 echo -e "\n \n \n git kopieren"
 echo "Ordner erstellen"
-mkdir /home/tobil/git1/arduino
-mkdir /home/tobil/git1/config
-mkdir /home/tobil/git1/doku
-mkdir /home/tobil/git1/latex_vorlage
+mkdir -p /home/tobil/git1/arduino
+mkdir -p /home/tobil/git1/config
+mkdir -p /home/tobil/git1/doku
+mkdir -p /home/tobil/git1/latex_vorlage
 
 echo -e "\n \n \n Dateien clonen"
 git clone https://github.com/tobil939/doku.git /home/tobil/git1/doku
 git clone https://github.com/tobil939/config.git /home/tobil/git1/config
 git clone https://github.com/tobil939/arduino.git /home/tobil/git1/arduino
-git clone https://github.com/tobil939/latex_vorlage /home/tobil/git1/latex_vorlage
+git clone https://github.com/tobil939/latex_vorlage.git /home/tobil/git1/latex_vorlage
 
+# Dateien und Verzeichnisse verschieben
 echo -e "\n \n \n Dateien verschieben"
 cd /home/tobil/git1/config/
-if [ ! -d "~/.config/nvim/"]; then
-  mkdir ~/.config/nvim/
-fi
 
-if [ ! -d "~/.config/nvim/lua/"]; then
-  mkdir ~/.config/nvim/lua/
-fi
+# Verzeichnisse für nvim und i3 erstellen, falls nicht vorhanden
+mkdir -p ~/.config/nvim/lua/config/
+mkdir -p ~/.config/nvim/lua/plugins/
+mkdir -p ~/.config/i3/
+mkdir -p ~/.config/gtk-3.0/
+mkdir -p ~/.config/gtk-4.0/
 
-if [ ! -d "~/.config/nvim/lua/config/"]; then
-  mkdir ~/.config/nvim/lua/config/
-fi
-
-if [ ! -d "~/.config/nvim/lua/plugins/"]; then
-  mkdir ~/.config/nvim/lua/plugins/
-fi
-
-if [ ! -d "~/.config/i3/"]; then
-  mkdir ~/.config/i3/
-fi
-
-if [ ! -d "~/.config/gtk-3.0/"]; then
-  mkdir ~/.config/gtk-3.0/
-fi
-
-if [ ! -d "~/.config/gtk-4.0/"]; then
-  mkdir ~/.config/gtk-4.0/
-fi
-
-if [ -d "~/.config/nvim/init.lua"]; then
+# Dateien verschieben und alte löschen
+if [ -f ~/.config/nvim/init.lua ]; then
   rm ~/.config/nvim/init.lua
 fi
 cp init.lua ~/.config/nvim/init.lua
 
-if [ -d "~/.config/nvim/lua/plugins/example.lua/"]; then
-  rm ~/.config/nvim/lua/plugins/example.lua 
+if [ -f ~/.config/nvim/lua/plugins/example.lua ]; then
+  rm ~/.config/nvim/lua/plugins/example.lua
 fi
 cp example.lua ~/.config/nvim/lua/plugins/example.lua
 
-if [ -d "~/.config/nvim/lua/config/autocmds.lua"]; then
+if [ -f ~/.config/nvim/lua/config/autocmds.lua ]; then
   rm ~/.config/nvim/lua/config/autocmds.lua
 fi
 cp autocmds.lua ~/.config/nvim/lua/config/autocmds.lua
 
-if [ -d "~/.config/nvim/lua/config/keymaps.lua"]; then
+if [ -f ~/.config/nvim/lua/config/keymaps.lua ]; then
   rm ~/.config/nvim/lua/config/keymaps.lua
 fi
 cp keymaps.lua ~/.config/nvim/lua/config/keymaps.lua
 
-if [ -d "~/.config/nvim/lua/config/lazy.lua"]; then
+if [ -f ~/.config/nvim/lua/config/lazy.lua ]; then
   rm ~/.config/nvim/lua/config/lazy.lua
 fi
 cp lazy.lua ~/.config/nvim/lua/config/lazy.lua
 
-if [ -d "~/.config/nvim/nvim/lua/config/options.lua"]; then
+if [ -f ~/.config/nvim/lua/config/options.lua ]; then
   rm ~/.config/nvim/lua/config/options.lua
 fi
 cp options.lua ~/.config/nvim/lua/config/options.lua
 
-if [ -d "~/.config/i3/config"]; then
+if [ -f ~/.config/i3/config ]; then
   rm ~/.config/i3/config
 fi
 cp config_i3 ~/.config/i3/config
 
-if [ -d "~/.config/i3/.i3status.conf"]; then
+if [ -f ~/.config/i3/.i3status.conf ]; then
   rm ~/.config/i3/.i3status.conf
 fi
 cp i3status.conf ~/.config/i3/.i3status.conf
 
-if [ -d "~/.config/gtk-3.0/settings.ini"]; then
+if [ -f ~/.config/gtk-3.0/settings.ini ]; then
   rm ~/.config/gtk-3.0/settings.ini
 fi
 cp settings_gtk3.ini ~/.config/gtk-3.0/settings.ini
 
-if [ -d "~/.config/gtk-4.0/settings.ini"]; then
+if [ -f ~/.config/gtk-4.0/settings.ini ]; then
   rm ~/.config/gtk-4.0/settings.ini
 fi
 cp settings_gtk4.ini ~/.config/gtk-4.0/settings.ini
-
-
-
-
 
