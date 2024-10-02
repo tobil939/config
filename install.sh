cb31780d6e6d63024333205c6156cff58e88a435
tobil939 <tobias.leitz@gmx.de>
2024-10-02 19:08:03 +0200

    wieder korrektur

--- install.sh
+++ install.sh
@@ -1,51 +1,51 @@
 #!/bin/bash
 
 # Installation von verschiedenen Paketen
-echo -e "\n \n \n neofetch"
+echo -e "\n neofetch"
 sudo pacman -S neofetch --noconfirm
-echo -e "\n \n \n kitty"
+echo -e "\n kitty"
 sudo pacman -S kitty --noconfirm
-echo -e "\n \n \n dmenu"
+echo -e "\n dmenu"
 sudo pacman -S dmenu --noconfirm
-echo -e "\n \n \n firefox"
+echo -e "\n firefox"
 sudo pacman -S firefox --noconfirm
-echo -e "\n \n \n gedit"
+echo -e "\n gedit"
 sudo pacman -S gedit --noconfirm
-echo -e "\n \n \n ttf-meslo-nerd"
+echo -e "\n ttf-meslo-nerd"
 sudo pacman -S ttf-meslo-nerd --noconfirm
-echo -e "\n \n \n picom"
+echo -e "\n picom"
 sudo pacman -S picom --noconfirm
-echo -e "\n \n \n feh"
+echo -e "\n feh"
 sudo pacman -S feh --noconfirm
-echo -e "\n \n \n git"
+echo -e "\n git"
 sudo pacman -S git --noconfirm
-echo -e "\n \n \n timeshift"
+echo -e "\n timeshift"
 sudo pacman -S timeshift --noconfirm
-echo -e "\n \n \n xorg"
+echo -e "\n xorg"
 sudo pacman -S xorg --noconfirm
-echo -e "\n \n \n gtk"
+echo -e "\n gtk"
 sudo pacman -S gtk2 --noconfirm
 sudo pacman -S gtk3 --noconfirm
 sudo pacman -S gtk4 --noconfirm
-echo -e "\n \n \n Dark Theme"
+echo -e "\n Dark Theme"
 sudo pacman -S gnome-themes-extra --noconfirm
 sudo pacman -S arc-gtk-theme --noconfirm
-echo -e "\n \n \n lxappearance"
+echo -e "\n lxappearance"
 sudo pacman -S lxappearance --noconfirm
-echo -e "\n \n \n npm"
+echo -e "\n npm"
 sudo pacman -S npm --noconfirm
-echo -e "\n \n \n todo list"
+echo -e "\n todo list"
 sudo pacman -S endeavour --noconfirm
-echo -e "\n \n \n PDF"
+echo -e "\n PDF"
 sudo pacman -S evince --noconfirm
-echo -e "\n \n \n LaTeX"
+echo -e "\n LaTeX"
 sudo pacman -S texlive --noconfirm
 sudo pacman -S texmaker --noconfirm
-echo -e "\n \n \n Nitrogen"
+echo -e "\n Nitrogen"
 sudo pacman -S nitrogen --noconfirm
-echo -e "\n \n \n Kalender"
+echo -e "\n Kalender"
 sudo pacman -S gnome-calendar --noconfirm
-echo -e "\n \n \n Nvim"
+echo -e "\n Nvim"
 sudo pacman -S neovim --noconfirm
 
 # Update durchführen
@@ -86,52 +86,52 @@ mkdir -p ~/.config/gtk-4.0/
 
 # Dateien verschieben und alte löschen
 if [ -f ~/.config/nvim/init.lua ]; then
-  rm ~/.config/nvim/init.lua
+  sudo rm ~/.config/nvim/init.lua
 fi
-cp init.lua ~/.config/nvim/init.lua
+sudo cp init.lua ~/.config/nvim/init.lua
 
 if [ -f ~/.config/nvim/lua/plugins/example.lua ]; then
-  rm ~/.config/nvim/lua/plugins/example.lua
+  sudo rm ~/.config/nvim/lua/plugins/example.lua
 fi
-cp example.lua ~/.config/nvim/lua/plugins/example.lua
+sudo cp example.lua ~/.config/nvim/lua/plugins/example.lua
 
 if [ -f ~/.config/nvim/lua/config/autocmds.lua ]; then
-  rm ~/.config/nvim/lua/config/autocmds.lua
+  sudo rm ~/.config/nvim/lua/config/autocmds.lua
 fi
-cp autocmds.lua ~/.config/nvim/lua/config/autocmds.lua
+sudo cp autocmds.lua ~/.config/nvim/lua/config/autocmds.lua
 
 if [ -f ~/.config/nvim/lua/config/keymaps.lua ]; then
-  rm ~/.config/nvim/lua/config/keymaps.lua
+  sudo rm ~/.config/nvim/lua/config/keymaps.lua
 fi
-cp keymaps.lua ~/.config/nvim/lua/config/keymaps.lua
+sudo cp keymaps.lua ~/.config/nvim/lua/config/keymaps.lua
 
 if [ -f ~/.config/nvim/lua/config/lazy.lua ]; then
-  rm ~/.config/nvim/lua/config/lazy.lua
+  sudo rm ~/.config/nvim/lua/config/lazy.lua
 fi
-cp lazy.lua ~/.config/nvim/lua/config/lazy.lua
+sudo cp lazy.lua ~/.config/nvim/lua/config/lazy.lua
 
 if [ -f ~/.config/nvim/lua/config/options.lua ]; then
-  rm ~/.config/nvim/lua/config/options.lua
+  sudo rm ~/.config/nvim/lua/config/options.lua
 fi
-cp options.lua ~/.config/nvim/lua/config/options.lua
+sudo cp options.lua ~/.config/nvim/lua/config/options.lua
 
 if [ -f ~/.config/i3/config ]; then
-  rm ~/.config/i3/config
+  sudo rm ~/.config/i3/config
 fi
-cp config_i3 ~/.config/i3/config
+sudo cp config_i3 ~/.config/i3/config
 
 if [ -f ~/.config/i3/.i3status.conf ]; then
-  rm ~/.config/i3/.i3status.conf
+  sudo rm ~/.config/i3/.i3status.conf
 fi
-cp i3status.conf ~/.config/i3/.i3status.conf
+sudo cp i3status.conf ~/.config/i3/.i3status.conf
 
 if [ -f ~/.config/gtk-3.0/settings.ini ]; then
-  rm ~/.config/gtk-3.0/settings.ini
+  sudo rm ~/.config/gtk-3.0/settings.ini
 fi
-cp settings_gtk3.ini ~/.config/gtk-3.0/settings.ini
+sudo cp settings_gtk3.ini ~/.config/gtk-3.0/settings.ini
 
 if [ -f ~/.config/gtk-4.0/settings.ini ]; then
-  rm ~/.config/gtk-4.0/settings.ini
+  sudo rm ~/.config/gtk-4.0/settings.ini
 fi
-cp settings_gtk4.ini ~/.config/gtk-4.0/settings.ini
+sudo cp settings_gtk4.ini ~/.config/gtk-4.0/settings.ini
 
