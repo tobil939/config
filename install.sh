19c460c1ad9e4504a9aed11f8114951cd3055a81
tobil939 <tobias.leitz@gmx.de>
2024-10-03 21:56:02 +0200

    user namen muss eingegeben werden

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+echo -e "Benutzername: ?"
+user_name=$(whoami)
+echo -e "\n Benutzername: $user_name \n"
+read -p "Benutzername für git: " user_git
+echo -e "\n Benutzername für git: $user_git \n"
+read -p "Emailadresse für git: " user_email
+echo -e "\n Emailadresse für git: $user_email \n"
+
 # Installation von verschiedenen Paketen
-echo -e "\n neofetch"
+echo -e "\n \n \n neofetch"
 sudo pacman -S neofetch --noconfirm
 echo -e "\n \n \n i3wm"
 sudo pacman -S i3-wm --noconfirm
@@ -21,6 +29,10 @@ echo -e "\n feh"
 sudo pacman -S feh --noconfirm
 echo -e "\n git"
 sudo pacman -S git --noconfirm
+echo -e "\n \n \n git cli"
+sudo pacman -S github-cli --noconfirm
+echo -e "\n \n \n wayland"
+sudo pacman -S wayland --noconfirm
 echo -e "\n \n \n nautilus"
 sudo pacman -S nautilus --noconfirm
 echo -e "\n timeshift"
@@ -54,9 +66,9 @@ echo -e "\n \n \n i3status"
 sudo pacman -S i3status --noconfirm
 echo -e "\n Nvim"
 sudo pacman -S neovim --noconfirm
-echo -s "\n \n \n tree-sitter"
-sudo npm -install -g tree-sitter --noconfirm
-sudo npm -install -g tree-sitter-cli --noconfirm
+echo -e "\n \n \n tree-sitter"
+sudo npm install -g tree-sitter
+sudo npm install -g tree-sitter-cli
 
 # Update durchführen
 echo -e "\n \n \n Update"
@@ -64,28 +76,29 @@ sudo pacman -Syu --noconfirm
 
 # Git-Konfiguration
 echo -e "\n \n \n git config"
-git config --global user.name "tobil939"
-git config --global user.email "tobias.leitz@gmx.de"
+git config --global user.name "$user_git"
+git config --global user.email "$user_email"
 git config --global core.editor nvim
 git config --list
 
 # Git-Ordner erstellen und klonen
 echo -e "\n \n \n git kopieren"
 echo "Ordner erstellen"
-mkdir -p /home/tobil/git1/arduino
-mkdir -p /home/tobil/git1/config
-mkdir -p /home/tobil/git1/doku
-mkdir -p /home/tobil/git1/latex_vorlage
+mkdir -p /home/$user_name/git1/arduino
+mkdir -p /home/$user_name/git1/config
+mkdir -p /home/$user_name/git1/doku
+mkdir -p /home/$user_name/git1/latex_vorlage
+mkdir -p /home/$user_name/git/
 
 echo -e "\n \n \n Dateien clonen"
-git clone https://github.com/tobil939/doku.git /home/tobil/git1/doku
-git clone https://github.com/tobil939/config.git /home/tobil/git1/config
-git clone https://github.com/tobil939/arduino.git /home/tobil/git1/arduino
-git clone https://github.com/tobil939/latex_vorlage.git /home/tobil/git1/latex_vorlage
+git clone https://github.com/tobil939/doku.git /home/$user_name/git1/doku
+git clone https://github.com/tobil939/config.git /home/$user_name/git1/config
+git clone https://github.com/tobil939/arduino.git /home/$user_name/git1/arduino
+git clone https://github.com/tobil939/latex_vorlage.git /home/$user_name/git1/latex_vorlage
 
 # Dateien und Verzeichnisse verschieben
 echo -e "\n \n \n Dateien verschieben"
-cd /home/tobil/git1/config/
+cd /home/$user_name/git1/config/
 
 # Verzeichnisse für nvim und i3 erstellen, falls nicht vorhanden
 echo -e "\n \n \n Ordner werden erstellt"
@@ -97,7 +110,7 @@ mkdir -p ~/.config/gtk-4.0/
 mkdir -p ~/.config/kitty/
 
 # Dateien verschieben und alte löschen
-cd /home/tobil/git1/config/
+cd /home/$user_name/git1/config/
 echo -e "\n \n \n init.lua"
 if [ -f ~/.config/nvim/init.lua ]; then
   sudo rm ~/.config/nvim/init.lua
@@ -109,7 +122,6 @@ if [ -f ~/.config/nvim/lua/plugins/example.lua ]; then
   sudo rm ~/.config/nvim/lua/plugins/example.lua
 fi
 sudo cp example.lua ~/.config/nvim/lua/plugins/example.lua
-
 
 echo -e "\n \n \n autocmds.lua"
 if [ -f ~/.config/nvim/lua/config/autocmds.lua ]; then
@@ -162,11 +174,23 @@ sudo cp settings_gtk4.ini ~/.config/gtk-4.0/settings.ini
 echo -e "\n \n \n kitty conf"
 if [ -f ~/.config/kitty/kitty.conf ]; then
   sudo rm ~/.config/kitty/kitty.conf
+fi
 sudo cp kitty.conf ~/.config/kitty/kitty.conf
+
+echo -e "\n \n \n bash"
+if [ -f /usr/local/bin/tex.sh ]; then
+  sudo rm /usr/local/bin/tex.sh
+fi
+
+cd /home/$user_name/git1/latex_vorlage
+sudo cp tex.sh /usr/local/tex.sh
+chmod +x tex.sh
+
+echo -e "\n \n \n Update"
+sudo pacman -Syu --noconfirm
 
 echo -e "\n \n \n Dark Mode"
 gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
 gsettings set org.gnome.desktop.interface icon-theme "Arc"
 gsettings set org.gnome.shell.extensions.user-theme name "Arc-Dark"
 echo -e "\n \n \n fertig"
-
