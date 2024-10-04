03d9b7c8aba1ee6d03ba5dbb9fe64638073c0d3e
tobil939 <tobias.leitz@gmx.de>
2024-10-04 20:09:28 +0200

    Update install.sh
    kttps error

--- install.sh
+++ install.sh
@@ -84,7 +84,7 @@ git config --list
 # Installation von yay
 echo -e "\n \n \n yay"
 sudo pacman -S --needed git base-devel
-git clone kttps://aur.archlinux.org/yay.git
+git clone https://aur.archlinux.org/yay.git
 cd yay 
 makepkg -si
 cd ~
