b176a9d52a1150054196d026d73120e17fe667b4
Tobi L <tobias.leitz@gmx.de>
2025-03-02 22:09:09 +0100

    Update the system
    performs an update, from archlinux-keyring, then pacman, then yay

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "Benutzername: ?"
+user_name=$(whoami)
+LOG_FILE="/home/$user_name/log/install.log"
+ERROR_LOG_FILE="/home/$user_name/log/errorinstall.log"
+datef="$(date '+%Y-%m-%d %H:%M:%S')"
+echo -e "\n $datef Benutzername: $user_name \n" >>"$LOG_FILE" 2>>"$ERROR_LOG_FILE"
+
+echo -e "\n \n \n ------keyring update------"
+sudo pacman -Sy archlinux-keyring --noconfirm >>"$LOG_FILE" 2>>"$ERROR_LOG_FILE"
+
+echo -e "\n \n \n ------Pacman------"
+sudo pacman -Syu --noconfirm >>"$LOG_FILE" 2>>"$ERROR_LOG_FILE"
+
+echo -e "\n \n \n ------yay------"
+sudo yay -Syu --noconfirm >>"$LOG_FILE" 2>>"$ERROR_LOG_FILE"
+#reboot
